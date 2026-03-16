e11b44c9dc30d5319ce6d5fdd9b3bfd0b9236a99
jes-bro <jes@ericbrown.net>
2026-03-16 12:51:10 -0700

    Get poses and comments

--- mutlivector_impl.py
+++ mutlivector_impl.py
@@ -1,27 +1,29 @@
 import json
 import subprocess
 from qdrant_client import QdrantClient, models
-
+from PIL import Image
+from ultralytics import YOLO
+import random
 # 1. Connect to Qdrant server
 client = QdrantClient(":memory:")
 
 from fastembed import TextEmbedding, LateInteractionTextEmbedding
-
+import os
 # from src.models.qwen3_vl_embedding import Qwen3VLEmbedder
 # embed the good and bad lists and store as separate vectors
-process = subprocess.Popen(['bash', '/home/jess/Qwen3-VL-Embedding/run_get_embeddings.sh'], stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT,
-    text=True)
+# process = subprocess.Popen(['bash', '/home/jess/Qwen3-VL-Embedding/run_get_embeddings.sh'], stdout=subprocess.PIPE,
+#     stderr=subprocess.STDOUT,
+#     text=True)
 
 
 log = []
 
-for line in process.stdout:
-    print(line, end="")
-    log.append(line)
+# for line in process.stdout:
+#     print(line, end="")
+#     log.append(line)
 
-process.wait()
-goodbadlistfile = '/home/jess/Qwen3-VL-Embedding/goodbadoutputs.json'
+# process.wait()
+goodbadlistfile = '/home/jess/Qwen3-VL-Embedding/test_spacy.json'
 
 # Define a list of query texts
 with open(goodbadlistfile, 'r') as file:
@@ -37,17 +39,38 @@ documents = []
 queries = []
 exp_video_names = []
 query_vid_names = []
-
-for video_name in goods_and_bads.keys():
-    if 'nov' not in video_name:
-        documents.append(goods_and_bads[video_name]['good'])
-        exp_video_names.append(video_name)
-        print("doc added")
-    else:
-        queries.append(goods_and_bads[video_name]['bad'])
-        queries.append(goods_and_bads[video_name]['good'])
-        print("query added")
-        query_vid_names.append(video_name)
+pose_model = YOLO("yolov8l-pose.pt")
+for subtask in goods_and_bads.keys():
+    for vid_name in goods_and_bads[subtask]:
+        if 'time_stamps_file_paths_poses' in goods_and_bads[subtask][vid_name].keys():
+           for timewindow in goods_and_bads[subtask][vid_name]['time_stamps_file_paths_poses']:
+                # print(goods_and_bads[subtask][vid_name]['time_stamps_file_paths_poses'][timewindow].keys()) # only need to check one because they always co-exist- double check
+                if "good_executions_list" in goods_and_bads[subtask][vid_name]['time_stamps_file_paths_poses'][timewindow].keys():
+                    good_executions_list = goods_and_bads[subtask][vid_name]['time_stamps_file_paths_poses'][timewindow]["good_executions_list"]
+                    needs_improvement_list = goods_and_bads[subtask][vid_name]['time_stamps_file_paths_poses'][timewindow]["needs_improvement_list"]
+                for camera_angle_clip in goods_and_bads[subtask][vid_name]['time_stamps_file_paths_poses'][timewindow]:
+                    if 'commentary' not in camera_angle_clip and 'good_executions_list' not in camera_angle_clip and 'needs_improvement_list' not in camera_angle_clip:
+                        for (frame_path, pose) in goods_and_bads[subtask][vid_name]['time_stamps_file_paths_poses'][timewindow][camera_angle_clip]:
+                            frame = Image.open(frame_path)
+                            # generate pose
+                            frame_pose_result = pose_model(frame) 
+                            # extract pose 
+                            pose = frame_pose_result[0].keypoints.data.cpu().numpy().tolist()
+                            print(pose)
+        # print(goods_and_bads[subtask][vid_name]['time_window'])
+        # print(goods_and_bads[subtask][vid_name]['time_window'].keys())
+    # print((subtask))
+    # print(goods_and_bads[subtask].keys())
+exit()
+    # if 'nov' not in video_name:
+    #     documents.append(goods_and_bads[video_name]['good'])
+    #     exp_video_names.append(video_name)
+    #     print("doc added")
+    # else:
+    #     queries.append(goods_and_bads[video_name]['bad'])
+    #     queries.append(goods_and_bads[video_name]['good'])
+    #     print("query added")
+    #     query_vid_names.append(video_name)
 
 print(f'documents: {documents}')
 print(f'queries: {queries}')
